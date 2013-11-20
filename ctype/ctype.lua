77841e7cf8c7c5d3adbf052347e4c87531227bfe
wtof1996 <wtof1996@gmail.com>
2013-11-20 21:34:01 +0800

    加入ctype中的ASCII字符表
    尚未未完成输入检查函数。

--- ctype/ctype.lua
+++ ctype/ctype.lua
@@ -15,6 +15,48 @@
    limitations under the License.
 ]]--
 
+
+--[[
+    Notice: This Lib ONLY ACCEPT SINGLE ASCII CHAR!(either num or string is ok.)
+
+    Each functions returns two value: res & err.
+    For testing functions, res can be true, false or nil.For converting functions, res can be result or nil.
+    When res is nil,it means an error occured,the err will contain a short discription.Otherwise the err will be nil.
+
+    _ctypeInput is a private function, which used to check your input and do some convert.
+]]--
+
+--[[
+    ASCII Table:
+
+    HEX         characters
+    0x00-0x08   control codes(NUL, etc.)
+    0x09        tab(\t)
+    0x0A-0x0D   whitspaces(\n,\v,\f,\r)
+    0x0E-0x1F   control codes
+    0x20        space
+    0x21-0x2F   !"#$%'()*+,-./
+    0x30-0x39   0123456789
+    0x3A-0x40   :'<=>?@
+    0x41-0x46   ABCDEF
+    0x47-0x5A   GHIJKLMNOPQRSTUVWXYZ
+    0x5B-0X60   [\]^_`
+    0x61-0x66   abcdef
+    0x67-0x7A   ghijkllmnopqrstuvwxyz
+    0x7B-0x7E   {|}~
+    0x7F        backspace character(DEL)
+]]--
+
+function _ctypeInput(input)
+    if(type(input) == "number") then
+        if((input > 0x7F) or (input < 0) or (frexp(input) ~= 0)  then return nil,"illegal character" end;
+        return input, nil;
+    elseif(type(input) == "string") then
+
+    else
+    end
+end
+
 function isalnum(char)
 
 end
