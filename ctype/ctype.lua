e12f6be6ef02f473ef43e3a56e14f5b5947abfd6
wtof1996 <wtof1996@gmail.com>
2013-11-21 15:25:20 +0800

    完成ctype输入检查函数
    添加了对assert的支持

--- ctype/ctype.lua
+++ ctype/ctype.lua
@@ -15,6 +15,56 @@
    limitations under the License.
 ]]--
 
+
+--[[
+    Notice: This Lib ONLY ACCEPT SINGLE ASCII CHAR!(either num or a string which contains one character is ok.)
+
+    Each functions returns two value: res & err.
+    For testing functions, res can be true, false or nil.For converting functions, res can be result or nil.
+    When res is nil,it means an error occured,the err will contain a short discription.Otherwise the err will be nil.
+    Suggestion:Use assert function to deal with error massage like this:
+        test_result = assert(isnumber("122"));
+
+    _ctypeInput is a private function, which used to check your input and do some convert.
+]]--
+
+--[[
+    ASCII Table:
+
+    HEX         characters
+    0x00-0x08   control codes(NUL, etc.)
+    0x09        tab(\t)
+    0x0A-0x0D   whitspaces(\n,\v,\f,\r)
+    0x0E-0x1F   control codes
+    0x20        space
+    0x21-0x2F   !"#$%'()*+,-./
+    0x30-0x39   0123456789
+    0x3A-0x40   :'<=>?@
+    0x41-0x46   ABCDEF
+    0x47-0x5A   GHIJKLMNOPQRSTUVWXYZ
+    0x5B-0X60   [\]^_`
+    0x61-0x66   abcdef
+    0x67-0x7A   ghijkllmnopqrstuvwxyz
+    0x7B-0x7E   {|}~
+    0x7F        backspace character(DEL)
+]]--
+
+--This private function is used to check input & convert all kinds of input into number.
+function ___ctypeCheckInput(input)
+    if(type(input) == "number") then
+        if((input > 0x7F) or (input < 0) or (math.ceil(input)) ~= input)  then return nil, "ctype:invalid character" end;
+        return input;
+    elseif(type(input) == "string") then
+        if(#input > 1) then return nil, "ctype:the string is too long" end;
+        local res = input:byte();
+        if((res > 0x7F) or (res < 0) or (math.ceil(res)) ~= res)  then return nil, "ctype:invalid character" end;
+        return res;
+    else
+        return nil, "ctype:invalid type";
+    end
+end
+
+--Public function
 function isalnum(char)
 
 end
