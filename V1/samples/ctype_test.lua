ac520f4106c0042a70290565c2c51291f7492831
wtof1996 <wtof1996@gmail.com>
2013-11-27 21:33:17 +0800

    修改了ctype的验证性样例

--- V1/samples/ctype_test.lua
+++ V1/samples/ctype_test.lua
@@ -3,3 +3,7 @@
 ]]--
 
 print(tostring(assert(ctype.isalnum("Z"))));
+print(tostring(assert(ctype.islower("Z"))));
+print(tostring(assert(ctype.isupper("Z"))));
+print(tostring(assert(ctype.isxdigit("A"))));
+print(tostring(assert(ctype.isprint("Z"))));
