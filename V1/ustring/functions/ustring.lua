5a6701a3b44238457b665be563c3daa68ebe7d4b
wtof1996 <wtof1996@gmail.com>
2013-12-16 21:24:23 +0800

    完成ubyte函数
    BUG已修复

--- V1/ustring/functions/ustring.lua
+++ V1/ustring/functions/ustring.lua
@@ -13,31 +13,38 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
+   NOTE:This Lib is contains some basic functions to deal with UTF8 string in TI-Lua.
+        If you want to use the Lua string-like class, please use the "ustring class".`
+        Because of the limition of string.uchar, this Lib only support U+0000 ~ U+FFFF(0~65535).
+        So the maximum of bytes in sequence is 3.
+    Functions List:
+
+    string.ubyte(uchar)
+        The UTF8 version of string.byte.
 ]]--
 
 __ustring = {};
-__ustring.MN = {["Flag1"] = 0x80, ["Flag2"] = 0xC0, ["Flag3"] = 0xE0};
---Magic Number
-__ustring.exception = {["invType"] = "ustring:invalid type"};
+__ustring.MN = {["Flag1"] = 0x80, ["Flag2"] = 0xC0, ["Flag3"] = 0xE0};--Magic Number
+__ustring.exception = {["invType"] = "ustring:invalid type"};--The exception list
 
-function string.ubyte(str)
-    if(type(str) ~= "string") then
-        error(ustring.exception.invType);
+function string.ubyte(uchar)
+    if(type(uchar) ~= "string") then
+       return nil, ustring.exception.invType;
     end
-    local a, b, c, res = str:byte(1), str:byte(2), str:byte(3), 0;
+    local a, b, c, res = uchar:byte(1), uchar:byte(2), uchar:byte(3), 0;
 
     if(a < __ustring.MN.Flag1) then
         res = a;
     else
-        if(a > __ustring.MN.Flag3) then
+        if(a >= __ustring.MN.Flag3) then
             res = (a - __ustring.MN.Flag3) * 4096 + (b - __ustring.MN.Flag1) * 64 + (c - __ustring.MN.Flag1);
         else
             res = (a - __ustring.MN.Flag2) * 64 + (b - __ustring.MN.Flag1);
         end
     end
 
-
-    return res;
+    return res, nil;
 end
 
 function string.ulen(str)
