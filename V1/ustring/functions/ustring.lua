452a7bb3ffe107cc59e0e66bf06b02c8848e7081
wtof1996 <wtof1996@gmail.com>
2013-12-17 21:22:41 +0800

    完成ulen函数

--- V1/ustring/functions/ustring.lua
+++ V1/ustring/functions/ustring.lua
@@ -18,10 +18,15 @@
         If you want to use the Lua string-like class, please use the "ustring class".`
         Because of the limition of string.uchar, this Lib only support U+0000 ~ U+FFFF(0~65535).
         So the maximum of bytes in sequence is 3.
+        For more detials of UTF8, please visit http://en.wikipedia.org/wiki/UTF-8
+
+
     Functions List:
 
     string.ubyte(uchar)
         The UTF8 version of string.byte.
+    string.ulen(str)
+        The UTF8 version of string.len.
 ]]--
 
 __ustring = {};
@@ -30,7 +35,7 @@ __ustring.exception = {["invType"] = "ustring:invalid type"};--The exception lis
 
 function string.ubyte(uchar)
     if(type(uchar) ~= "string") then
-       return nil, ustring.exception.invType;
+       return nil, __ustring.exception.invType;
     end
     local a, b, c, res = uchar:byte(1), uchar:byte(2), uchar:byte(3), 0;
 
@@ -48,7 +53,28 @@ function string.ubyte(uchar)
 end
 
 function string.ulen(str)
+    if(type(str) ~= "string") then
+       return nil, __ustring.exception.invType;
+    end
 
+    local bytes, res, index = #str, 0, 1;
+    local tmp_byte;
+
+    while(index <= bytes) do
+        tmp_byte = str:byte(index);
+        if(tmp_byte < __ustring.MN.Flag1) then
+            index = index + 1;
+        else
+            if(tmp_byte >= __ustring.MN.Flag3) then
+                index = index + 3;
+            else
+                index = index + 2;
+            end
+        end
+        res = res + 1;
+    end
+
+    return res, nil;
 end
 
 function string.UTF8CharLen(num)
